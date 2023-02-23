173a092c4f9d1801396ffdcff7209ecf07d8e06c
iamsamuel88 <124935521+iamsamuel88@users.noreply.github.com>
2023-02-23 14:25:45 -0800

    Create js
    Created scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="author" content="Samuel Huynh">
+    <meta name="description" content="My UX Portfolio">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,600;1,400&display=swap" rel="stylesheet">
+    <title>Samuel V Huynh</title>
+  </head>
+  console.log(Wassup Peeps)
